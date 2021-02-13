ad76a43726f40c9349e0d244acecbe320430b6cc
irfan1775 <irfan.maulana@students.amikom.ac.id>
2021-02-13 07:11:16 +0700

    latian

--- src/Component/Class/Produk.jsx
+++ src/Component/Class/Produk.jsx
@@ -19,7 +19,7 @@ ButtonBeli = () => {
         stock: this.state.stock -1
     })
 
-    if(this.state.stock ===1){
+    if(this.state.stock === 1){
         this.setState({
             status: "sold out",
             disable: "true"
@@ -33,7 +33,7 @@ ButtonBeli = () => {
             <div className="box-stock">
                 <h2>{this.props.nama}</h2>
                 <img src="https://placeimg.com/640/480/animals" alt=""/>
-                <p>{this.props.harga}</p>
+                <p><b>RP. {this.props.harga}</b> </p>
                 <p>{this.state.stock}</p>
             <button className="btn-click" onClick={this.ButtonBeli} disabled={this.state.disable}> Beli </button>
                 <p>{this.state.status}</p>
--- src/Component/Fungsional/Home.jsx
+++ src/Component/Fungsional/Home.jsx
@@ -6,23 +6,12 @@ import Blog from './Blog'
 
 const Home = () => {
     return <div>
-        {<Produk nama="anjing" stock = "10" harga = "10000000" />/* <Blog 
-        Tanggal = "13 februari 2021"
-        Judul = "mencoba aplikasi"
-        Summary = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel veritatis placeat non quibusdam, quam nesciunt harum in? Distinctio dolores placeat neque sed at aliquid facere soluta magni, voluptatum laudantium reiciendis!"
-        />
-        
-        <Blog 
-        Tanggal = "13 februari 2021"
-        Judul = "React"
-        Summary = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel veritatis placeat non quibusdam, quam nesciunt harum in? Distinctio dolores placeat neque sed at aliquid facere soluta magni, voluptatum laudantium reiciendis!"
-        />
-
-        <Blog 
-        Tanggal = "13 februari 2021"
-        Judul = "React native"
-        Summary = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel veritatis placeat non quibusdam, quam nesciunt harum in? Distinctio dolores placeat neque sed at aliquid facere soluta magni, voluptatum laudantium reiciendis!"
-        /> */}
+        <Produk nama="anjing" stock = "10" harga = "10000000" />
+        <Produk nama="anjing" stock = "10" harga = "10000000" />
+        <Produk nama="anjing" stock = "10" harga = "10000000" />
+        <Produk nama="anjing" stock = "10" harga = "10000000" />
+        <Produk nama="anjing" stock = "10" harga = "10000000" />
+        <Produk nama="anjing" stock = "10" harga = "10000000" />
     </div>
 }
 
