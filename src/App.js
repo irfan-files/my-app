e132f961f65b0dab403df5458b4bab8d2728e378
irfan <irfan.maulana@students.amikom.ac.id>
2021-02-23 17:38:29 +0700

    passing data

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import NavbarComp from './Component/Fungsional/NavbarComp';
 import HomePage from './Component/Fungsional/HomePage';
 import About from './Component/Fungsional/AboutComp';
+import DetailComp from './Component/Fungsional/DetailComp';
 
 // import Beranda from './Component/Class/Beranda';
 
@@ -17,6 +18,7 @@ const App = () => {
     <Switch>
       <Route exact path="/" component={HomePage}/>
       <Route exact path="/about" component={About}/>
+      <Route exact path="/detail/:id" component={DetailComp}/>
     </Switch>
     </BrowserRouter>
   );
--- src/Component/Fungsional/AboutComp.jsx
+++ src/Component/Fungsional/AboutComp.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Jumbotron, Button } from 'reactstrap';
+import { Jumbotron, Button, Container, Row, Col } from 'reactstrap';
+import CardComp from './CardComp';
+
 
 function AboutComp() {
   return (
@@ -14,6 +16,15 @@ function AboutComp() {
           <Button color="primary">Learn More</Button>
         </p>
       </Jumbotron>
+
+      <Row>
+        <Col><CardComp id="1" judul="Perpustakaan" tanggal="22/06/2929" keterangan="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla molestias mollitia beatae, delectus fugit velit sequi fuga eligendi tempore. Odit veniam totam cupiditate iure commodi alias error illum aperiam voluptatum?" /></Col>
+        <Col><CardComp id="2" judul="Tatanan Buku" tanggal="22/06/2930" keterangan="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla molestias mollitia beatae, delectus fugit velit sequi fuga eligendi tempore. Odit veniam totam cupiditate iure commodi alias error illum aperiam voluptatum?"/></Col>
+        <Col><CardComp id="3" judul="Perbukuan" tanggal="22/06/2019" keterangan="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla molestias mollitia beatae, delectus fugit velit sequi fuga eligendi tempore. Odit veniam totam cupiditate iure commodi alias error illum aperiam voluptatum?"/></Col>
+      </Row>
+      
+
+
     </div>
     
   )
